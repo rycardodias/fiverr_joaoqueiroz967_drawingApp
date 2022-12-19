931c2698a818196bc662d41f7e0d98abe0ec2d4c
Ricardo Dias <rycardo.dias@hotmail.com>
2022-12-19 19:07:20 +0000

    spray size

--- scripts/modes/spray.js
+++ scripts/modes/spray.js
@@ -3,7 +3,7 @@ import { getDrawingColor, getLineSize } from '../userPreferences.js'
 export const setSprayMode = (canvas) => {
     canvas.freeDrawingBrush = new fabric.SprayBrush(canvas);
     canvas.freeDrawingBrush.color = getDrawingColor();
-    canvas.freeDrawingBrush.width = getLineSize()
+    canvas.freeDrawingBrush.width = getLineSize() * 10
 
     canvas.isDrawingMode = true;
     canvas.renderAll();
